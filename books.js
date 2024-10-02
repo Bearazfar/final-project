cc49012227a78a3ccabe973ef94770dbabf7bd11
ฺBearazfar <124041758+Bearazfar@users.noreply.github.com>
2024-10-02 20:47:50 +0700

    books.js

--- books.js
+++ books.js
@@ -5,149 +5,88 @@
       "title": "A home that collects loneliness and stories that no one ever hears",
       "author": "Machida Sonoko",
       "coverImage": "assets/picture/บ้านสะสมความเหงา-768x768.png",
-      "category": "Fiction", 
-      "yearPublished": "2020", 
+      "category": "Romance",
+      "yearPublished": "2020",
       "isbn": "978-1234567890"
-  },
-  {
+    },
+    {
       "id": "db70",
       "title": "Our lives are just as bitter as this",
       "author": "Lee Kyung-ran",
       "coverImage": "assets/picture/sg-11134201-7rdvy-m0cchq9lpi2x8d.jpg",
-      "category": "Fiction", 
-      "yearPublished": "2018", 
+      "category": "Fiction",
+      "yearPublished": "2018",
       "isbn": "978-1234567891"
-  },
-  {
+    },
+    {
       "id": "9bdf",
       "title": "The Menu of Hope at the Seaside Café",
       "author": "Takashi Kitajima",
       "coverImage": "assets/picture/sg-11134201-7rdw1-lzl8b8xb80hg17-768x768.jpg",
-      "category": "Fiction", 
-      "yearPublished": "2021", 
+      "category": "Fiction",
+      "yearPublished": "2021",
       "isbn": "978-1234567892"
-  },
-  {
+    },
+    {
       "id": "9f99",
       "title": "Let a warm meal embrace your heart first",
       "author": "Takeuchi Masami",
       "coverImage": "assets/picture/sg-11134201-7rdyd-lzco6t4p3dhie4-768x768.jpg",
-      "category": "romance", 
-      "yearPublished": "2019", 
+      "category": "romance",
+      "yearPublished": "2019",
       "isbn": "978-1234567893"
-  },
-  {
+    },
+    {
       "id": "8a5f",
       "title": "The Snack Waiting with the Last Request",
       "author": "Ogawa Ito",
       "coverImage": "assets/picture/81DB93EC-CCFC-472A-8F71-ACA4095BBE86.png",
-      "category": "Mystery", 
-      "yearPublished": "2022", 
+      "category": "Mystery",
+      "yearPublished": "2022",
       "isbn": "978-1234567894"
-  },
-  {
+    },
+    {
       "id": "2529",
       "title": "The Secret Curry Recipe and the Mystery in the Book Café",
       "author": "Yanase Michiru",
       "coverImage": "assets/picture/cddtsiujl^9iy-768x768.png",
-      "category": "Mystery", 
-      "yearPublished": "2023", 
+      "category": "Mystery",
+      "yearPublished": "2023",
       "isbn": "978-1234567895"
-  },
-  {
+    },
+    {
       "id": "1e6c",
       "title": "The House with the Lying Cat and the Dog in Imagination",
       "author": "Teraji Haruna",
       "coverImage": "assets/picture/แมวขี้โกหก.png",
-      "category": "Fantasy", 
-      "yearPublished": "2020", 
+      "category": "Fantasy",
+      "yearPublished": "2020",
       "isbn": "978-1234567896"
-  },
-  {
+    },
+    {
       "id": "4462",
       "title": "The Umbrella Shop: Coffee Service and Various Odd Jobs",
       "author": "Teraji Haruna",
       "coverImage": "assets/picture/ร้านร่ม.png",
-      "category": "Fiction", 
-      "yearPublished": "2021", 
+      "category": "Fiction",
+      "yearPublished": "2021",
       "isbn": "978-1234567897"
-  },
-    {
-      "title": "Dertiiii",
-      "author": "asdasdas",
-      "category": "whatasdadadads",
-      "yearpubish": "124544",
-      "isbn": "asd61661",
-      "id": "dbaa"
     },
     {
-      "title": "DAncing",
-      "author": "Athur",
-      "category": "74",
-      "yearpubish": "11215",
-      "isbn": "747474",
-      "id": "c45d"
+      "id": "e2b4",
+      "title": "SWessa",
+      "author": "ASDD",
+      "category": "Fantacy",
+      "yearpubish": "54",
+      "isbn": "asda5"
     },
     {
-      "title": "Awewewewewewe",
-      "author": "sdgaf",
-      "category": "wegef",
-      "yearpubish": "21121",
-      "isbn": "12e11dsf",
-      "id": "3d97"
-    },
-    {
-      "id": "6347",
-      "title": "Kasssss",
-      "author": "kasss",
-      "category": "ADSASF",
-      "yearpubish": "12141",
-      "isbn": "sfddfas1f5a1f",
-      "coverImage": ""
-    },
-    {
-      "id": "949c",
-      "title": "asasa",
-      "author": "asas",
-      "category": "asas",
-      "yearpubish": "asas",
-      "isbn": "asas",
-      "coverImage": ""
-    },
-    {
-      "title": "asdad",
-      "author": "asdeeeeeeeeee",
-      "category": "aads",
-      "yearpubish": "dada",
-      "isbn": "adasda",
-      "id": "3163"
-    },
-    {
-      "id": "8fb7",
-      "title": "ฟหกฟหก",
-      "author": "ฟหกฟหก",
-      "category": "หฟกฟหก",
-      "yearpubish": "ฟฟหกฟหก",
-      "isbn": "หฟกฟหก",
-      "coverImage": ""
-    },
-    {
-      "id": "7ee5",
-      "title": "sadasd",
-      "author": "asdasdasdasda",
-      "category": "asdasdasda",
-      "yearpubish": "",
-      "isbn": "asdasdasdads",
-      "coverImage": ""
-    },
-    {
-      "id": "71ef",
-      "title": "helloooooooooooooooo",
-      "author": "meaowww",
-      "category": "romance",
-      "yearpubish": "2021",
-      "isbn": "00000",
-      "coverImage": ""
+      "id": "8369",
+      "title": "adasd",
+      "author": "dedwwdq",
+      "category": "Romance",
+      "yearpubish": "w5151",
+      "isbn": "a4sd1asd"
     }
   ]
 }
